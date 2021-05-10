77b5aac8e53197c3fb5e478ae30537d3acb28350
Alfian <alfianyuda191@gmail.com>
2021-05-10 23:57:13 +0700

    IcaCraft

---
+++ app/src/main/java/com/example/icacraft/ui/home/HomeFragment.kt
@@ -0,0 +1,51 @@
+package com.example.icacraft.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.icacraft.R
+import com.example.icacraft.model.dummy.HomeModel
+import kotlinx.android.synthetic.main.fragment_home.*
+import java.util.ArrayList
+
+class HomeFragment : Fragment(), HomeAdapter.ItemAdapterCallback{
+
+  private var craftlist : ArrayList<HomeModel> = ArrayList()
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    val root = inflater.inflate(R.layout.fragment_home, container, false)
+    return root
+  }
+
+  override fun onActivityCreated(savedInstanceState: Bundle?) {
+    super.onActivityCreated(savedInstanceState)
+
+    initDataDummy()
+    var adapter = HomeAdapter(craftlist, this)
+    var layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+    rclist.layoutManager = layoutManager
+    rclist.adapter = adapter
+  }
+
+  fun initDataDummy() {
+      craftlist = ArrayList()
+      craftlist.add(HomeModel("Starp Mask", "", 5f))
+      craftlist.add(HomeModel("Gantungan Kunci", "", 3f))
+      craftlist.add(HomeModel("Dream Catcher", "", 4f))
+  }
+
+  override fun onClick(v: View, data: HomeModel) {
+    TODO("Not yet implemented")
+  }
+}
